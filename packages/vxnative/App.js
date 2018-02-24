04ae9d2e15fdd958b83ebd9e49b2db169dd7bad6
Jason Brown <browniefed@gmail.com>
2018-02-24 11:11:42 -0800

    Fix more

--- packages/vxnative/App.js
+++ packages/vxnative/App.js
@@ -3,20 +3,66 @@ import { StyleSheet, Text, View, Dimensions } from "react-native";
 import * as RNSvg from "react-native-svg";
 import { buildInject } from "@vx/primitives";
 import * as Shape from "@vx/shape";
+import { Group } from "@vx/group";
+import { letterFrequency } from '@vx/mock-data';
+import { scaleBand, scaleLinear } from '@vx/scale';
+import { extent, max } from 'd3-array';
 buildInject(RNSvg);
 
 const { width, height } = Dimensions.get("window");
 
 const Svg = RNSvg.Svg;
 
-console.log(Shape.Line);
+const data = letterFrequency.slice(5);
+
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+// accessors
+const x = d => d.letter;
+const y = d => +d.frequency * 100;
+
 
 export default class App extends Component {
   render() {
+    const xMax = width;
+    const yMax = height - 120;
+
+    // scales
+    const xScale = scaleBand({
+      rangeRound: [0, xMax],
+      domain: data.map(x),
+      padding: 0.4,
+    });
+    const yScale = scaleLinear({
+      rangeRound: [yMax, 0],
+      domain: [0, max(data, y)],
+    });
     return (
       <View style={styles.container}>
         <Svg width={width} height={height}>
-          <Shape.Line from={{ x: 0, y: 0 }} to={{ x: 100, y: 100 }} stroke="red" strokeWidth="2" />
+          <Group top={40}>
+            {data.map((d, i) => {
+              const barHeight = yMax - yScale(y(d));
+              return (
+                <Group key={`bar-${x(d)}`}>
+                  <Shape.Bar
+                    width={xScale.bandwidth()}
+                    height={barHeight}
+                    x={xScale(x(d))}
+                    y={yMax - barHeight}
+                    fill="rgba(23, 233, 217, .5)"
+                    data={{ x: x(d), y: y(d) }}
+                    onClick={data => event => {
+                      alert(`clicked: ${JSON.stringify(data)}`);
+                    }}
+                  />
+                </Group>
+              );
+            })}
+          </Group>
         </Svg>
       </View>
     );
